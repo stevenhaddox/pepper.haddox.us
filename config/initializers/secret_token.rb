921bcff23205a2981c25fcec36153c8c1f13072d
Steven Haddox <steven@haddox.us>
2013-05-25 16:30:42 -0400

    Initial Rails 4.0 Application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pepper::Application.config.secret_key_base = '03baecaf9c1a29512d1ffab4d1ace31d9b750979ec1a7e4abb571a4e3dc51bc37179b6e1ac72b02369c77eaee1e86828d9aca1c83d0ebb1b0e37028b7f4ca2dd'
